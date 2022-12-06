cec8050ecd455780df82615983f654623a6ce83a
heesu_Yeo <gmltnwkd123@naver.com>
2022-12-06 22:39:08 +0900

    Add wash method to apple

--- src/fruit/Apple.java
+++ src/fruit/Apple.java
@@ -2,8 +2,15 @@ package fruit;
 
 public class Apple {
 	int price;
+	int grade;
+	
+	boolean clean;
 	
 	public void eat() {
 		price -= 1000;
+	}
+	public void wash() {
+		
+		clean = true;
 	}
 }
